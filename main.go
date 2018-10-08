1dadfc543cd05488d64d7a86d55f0e9db841b551
Brandon Armstrong <brandon.armstrong@thingco.com>
2018-10-08 11:37:07 +0100

    final recovery program

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"net"
 	"runtime/debug"
 	"fmt"
 	"log"
@@ -55,6 +57,22 @@ func(rw *responseWriter) Write(b []byte) (int, error){
 
 func(rw *responseWriter) WriteHeader(statusCode int){
 	rw.status = statusCode
+}
+
+func(rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hijacker, ok := rw.ResponseWriter.(http.Hijacker)
+	if !ok{
+		return nil, nil, fmt.Errorf("the response writer does not support the hijacker interface")
+	}
+	return hijacker.Hijack()
+}
+
+func (rw *responseWriter) Flush() {
+	flusher, ok := rw.ResponseWriter.(http.Flusher)
+	if !ok {
+		return
+	}
+	flusher.Flush()
 }
 
 func (rw *responseWriter) flush() error{
